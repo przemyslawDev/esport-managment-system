54cb2cd74340b5bd3f7e9ae417eb083d08592c70
Przemysław Łapiński <przemyslaw.lapinski.dev@gmail.com>
2017-09-11 10:48:38 +0200

    attach admin and guest roles and create test users, gitignore change for vsc

---
+++ database/migrations/2017_09_11_083023_add_admin_and_guest_users.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\User;
+use App\Role;
+
+class AddAdminAndGuestUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('users')->truncate();
+
+        $guest_role = Role::find(1);
+        $system_admin_role = Role::find(2);
+
+        $user = new User();
+
+        $user->email = 'user@example.com';
+        $user->password = bcrypt('secret');
+        $user->active = true;
+        $user->save();
+
+        $user->attachRole($guest_role);
+
+        $admin = new User();
+        $admin->email = 'admin@example.com';
+        $admin->password = bcrypt('secret');
+        $admin->active = true;
+        $admin->save();
+
+        $admin->attachRole($system_admin_role);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
