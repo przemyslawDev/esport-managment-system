0fa3ce807714adab6254d9d7211fa0a2f77ea9ba
Przemysław Łapiński <przemyslaw.lapinski.dev@gmail.com>
2017-09-12 10:01:41 +0200

    travis

--- config/database.php
+++ config/database.php
@@ -91,21 +91,6 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        'testingg' => [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'port' => '3306',
-            'database' => 'homestead_test',
-            'username' => 'root',
-            'password' => '',
-            'unix_socket' => '',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ]
-
     ],
 
     /*
