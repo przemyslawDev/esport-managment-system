b215d0e00411fa9260309cc9f0d6e5d8e0206685
Przemysław Łapiński <przemyslaw.lapinski.dev@gmail.com>
2017-09-20 09:19:53 +0200

    create migrations and model for Games

---
+++ Modules/TeamManagment/Models/Game.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\TeamManagment\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    protected $fillable = ['name'];
+}
---
+++ Modules/TeamManagment/Models/Traits/GameTrait.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\TeamMangment\Models\Traits;
+
+trait GameTrait
+{
+    
+}
