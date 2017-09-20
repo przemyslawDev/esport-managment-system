fcd1b955ed4f6652597b13080e44f0cd8793fef5
Przemysław Łapiński <przemyslaw.lapinski.dev@gmail.com>
2017-09-20 10:00:50 +0200

    games - controller, tests, routes, views

---
+++ Modules/TeamManagment/Http/Controllers/GameController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\TeamManagment\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use Modules\TeamManagment\Models\Game;
+
+class GameController extends Controller
+{
+    public function index()
+    {
+        return view('teammanagment::games.index');
+    }
+
+    public function show($id)
+    {
+        return view('teammanagment::games.show')->with('id', $id);
+    }
+
+    public function get($id) 
+    {
+        $game = Game::findOrFail($id)->first();
+
+        return response()->json($game);
+    }
+}
--- Modules/TeamManagment/Http/routes.php
+++ Modules/TeamManagment/Http/routes.php
@@ -3,6 +3,11 @@
 Route::group(['middleware' => 'web', 'prefix' => 'teammanagment', 'namespace' => 'Modules\TeamManagment\Http\Controllers'], function()
 {
     Route::middleware('auth')->group(function () {
+        Route::prefix('games')->middleware('role:system_admin')->group(function () {
+            Route::get('/', 'GameController@index')->name('teammanagment.games.index');
+            Route::get('/{id}', 'GameController@show')->name('teammanagment.games.show');
 
+            Route::get('/game/{id}', 'GameController@get')->name('teammanagment.games.get');
+        });
     });
 });
