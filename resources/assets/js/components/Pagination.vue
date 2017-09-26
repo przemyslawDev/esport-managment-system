d1046cbe299b9e16b301f5b024776b45c0be5acd
Przemysław Łapiński <przemyslaw.lapinski.dev@gmail.com>
2017-09-26 20:43:51 +0200

    pagination component fix

--- resources/assets/js/components/Pagination.vue
+++ resources/assets/js/components/Pagination.vue
@@ -1,12 +1,12 @@
 <template>
     <ul v-if="info.total > 0" class="pagination">
         <li :class="info.current_page == 1 ? 'disabled' : ''">
-            <a v-on:click="data(info.current_page - 1)" aria-label="Previous">
+            <a @click="(info.current_page == 1 ? null : data(info.current_page - 1))" aria-label="Previous">
                 <span aria-hidden="true">&laquo;</span>
             </a>
         </li>
         <li v-if="info.current_page > 1">
-            <a v-on:click="data(info.current_page - 1)">{{ info.current_page - 1 }}
+            <a @click="data(info.current_page - 1)">{{ info.current_page - 1 }}
                 <span class="sr-only">(current)</span>
             </a>
         </li>
@@ -16,12 +16,12 @@
             </a>
         </li>
         <li v-if="info.current_page < info.last_page">
-            <a v-on:click="data(info.current_page + 1)">{{ info.current_page + 1 }}
+            <a @click="data(info.current_page + 1)">{{ info.current_page + 1 }}
                 <span class="sr-only">(current)</span>
             </a>
         </li>
         <li :class="info.current_page == info.last_page ? 'disabled' : ''">
-            <a v-on:click="data(info.current_page + 1)" aria-label="Next">
+            <a @click="(info.current_page == info.last_page ? null : data(info.current_page + 1))" aria-label="Next">
                 <span aria-hidden="true">&raquo;</span>
             </a>
         </li>
